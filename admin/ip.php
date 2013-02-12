9b229afa15188d6426e806331bf0f5eda7188680
msoni <mahendra20nov@gmail.com>
2013-02-12 21:28:56 +0530

    Modified ip.php : Misssed session check..

--- admin/ip.php
+++ admin/ip.php
@@ -3,7 +3,7 @@ include 'includes/_incHeader.php';
 $db = new cDB();
 
 if (!isset($_SESSION['username']) || $_SESSION['isadmin'] != 1) {
-//	header("Location:logout.php");
+	header("Location:logout.php");
 }
 ?>
 <!-- Navbar start -->
